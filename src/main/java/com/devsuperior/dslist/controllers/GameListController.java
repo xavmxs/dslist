a2b62ce74bde70aad2db8c0c2248099d7de857ce
xavmxs <xavmxspro@gmail.com>
2023-07-13 18:36:50 -0300

    projections and update database

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class GameListController {
 	@Autowired
 	private GameService gameService;
 	
-	
+	 
 	
 	@GetMapping
 	public List<GameListDTO> findAll(){
@@ -30,6 +31,10 @@ public class GameListController {
 		return result;
 	}
 	
-
+	@GetMapping(value="/{listId}/games")
+	public List<GameMinDTO> findByList(@PathVariable Long listId){
+		List <GameMinDTO> result = gameService.findByList(listId);
+		return result;
+	}
 
 }
