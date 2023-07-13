9ebdbea7bc828136c1820d79f7235cce4897fef4
xavmxs <xavmxspro@gmail.com>
2023-07-13 17:19:07 -0300

    GET lists

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -4,31 +4,32 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.service.GameListService;
 import com.devsuperior.dslist.service.GameService;
 
 @RestController
-@RequestMapping(value="/games")
-public class GameController {
+@RequestMapping(value="/list")
+public class GameListController {
+	
+	@Autowired
+	private GameListService gameListService;
 	
 	@Autowired
 	private GameService gameService;
 	
-	@GetMapping(value="/{id}")
-	public GameDTO findById(@PathVariable Long id){
-	GameDTO result = gameService.findById(id);
+	
+	
+	@GetMapping
+	public List<GameListDTO> findAll(){
+		List <GameListDTO> result = gameListService.findAll();
 		return result;
 	}
 	
-	@GetMapping
-	public List<GameMinDTO> findAll(){
-		List <GameMinDTO> result = gameService.findAll();
-		return result;
-	}
+
 
 }
